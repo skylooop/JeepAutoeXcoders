a9d5191e7e18e8402410bb699548a05a4dac42ee
m.bobrin <maxs.bobrin@gmail.com>
2023-02-27 06:36:17 +0000

    model

--- Inception/src/model.py
+++ Inception/src/model.py
@@ -12,3 +12,8 @@ import typing as tp
 class TrainState(train_state.TrainState):
     batch_stats: tp.Any
 
+class TrainerModule:
+    def __init__(self, model_name: str, model_class: nn.Module, 
+                 model_hparams:  dict, optimizer_name: str, 
+                 optimizer_hparams: dict, expmp_imgs: tp.Any, seed: int = 42) -> None:
+        
